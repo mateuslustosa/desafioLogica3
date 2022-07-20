c51d92056563cdd142a22c1fc5492eea61e1aa88
mateuslustosa <mateuslustosa.o@gmail.com>
2022-07-20 13:34:20 -0300

    Desafio 3 completo!

---
+++ script.js
@@ -0,0 +1,24 @@
+//Criar um loop que conte de 1 até 10 usando for
+for(let i = 1; i <= 10; i++){
+       console.log(i);
+}
+
+//Criar um loop que conte de 10 até 1 usando WHILE
+ let contadoDecrescente = 10;
+
+ while(contadorDescrescente >= 1){
+   console.log(contadorDescrescente--)
+}
+
+//Criar um loop que conte todos os númeors ímpares de 1 até 100
+ for(let impar = 1; impar <= 100; impar+2){
+   console.log(impar);
+}
+
+//Criar um loop que conte todos os números pares de 0 a 100
+ let number = 0;
+
+ while(number <= 100){
+   console.log(number)
+   number+=2;
+}
